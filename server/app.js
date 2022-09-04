2dbd188e96c1ef67c2d0f615dfea9db57942b8f1
sdennett55@gmail.com <sdennett55@gmail.com>
2022-09-04 18:25:34 -0400

    moved gameState into Context

--- server/app.js
+++ server/app.js
@@ -9,21 +9,22 @@ const io = require("socket.io")(5000, {
 });
 
 // the game state
-let gameState = {};
+let gameState = { players: {} };
 
 io.on("connect", function (socket) {
   console.log("A new client has joined", socket.id);
 
   // new player joined
-  gameState[socket.id] = {};
-  gameState[socket.id].count = 0;
+  gameState.players[socket.id] = {};
+  gameState.players[socket.id].count = 0;
+  gameState.players[socket.id].score = 0;
 
   // emit to the newly connected client the existing count
   socket.emit("gameState updated", gameState);
 
   // we listen for this event from the clients
   socket.on("counter clicked", (socketId) => {
-    gameState[socketId].count += 1;
+    gameState.players[socketId].count += 1;
     console.log("clicked!", socketId, gameState);
 
     // emit to EVERYONE the update game state
@@ -38,14 +39,25 @@ io.on("connect", function (socket) {
   socket.on("a player disconnected", () => {
     // emit to EVERYONE the update game state
     console.log("wtfwtfwtf player left!!!", socket.id);
-    delete gameState[socket.id];
+    delete gameState.players[socket.id];
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("player signed up", (playerName) => {
+    console.log("yo, this player signed up", playerName);
+    gameState.players[socket.id].name = playerName;
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("give updated game state", () => {
+    console.log("wtfwtfwtf");
     io.emit("gameState updated", gameState);
   });
 
   socket.on("disconnect", function () {
     console.log(socket.id + " disconnected!");
     // emit to EVERYONE the update game state
-    delete gameState[socket.id];
+    delete gameState.players[socket.id];
     io.emit("gameState updated", gameState);
   });
 });
