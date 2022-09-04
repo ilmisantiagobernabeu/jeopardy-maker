a517648f2dcdc5b288e8ba3c9ab7d28f2de49e2b
sdennett55@gmail.com <sdennett55@gmail.com>
2022-09-04 12:31:08 -0400

    adding websockets!

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const io = require("socket.io")(5000, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+  "sync disconnect on unload": true,
+});
+
+// the game state
+let gameState = {};
+
+io.on("connect", function (socket) {
+  console.log("A new client has joined", socket.id);
+
+  // new player joined
+  gameState[socket.id] = {};
+  gameState[socket.id].count = 0;
+
+  // emit to the newly connected client the existing count
+  socket.emit("gameState updated", gameState);
+
+  // we listen for this event from the clients
+  socket.on("counter clicked", (socketId) => {
+    gameState[socketId].count += 1;
+    console.log("clicked!", socketId, gameState);
+
+    // emit to EVERYONE the update game state
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("new player joined", () => {
+    // emit to EVERYONE the update game state
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("a player disconnected", () => {
+    // emit to EVERYONE the update game state
+    console.log("wtfwtfwtf player left!!!", socket.id);
+    delete gameState[socket.id];
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("disconnect", function () {
+    console.log(socket.id + " disconnected!");
+    // emit to EVERYONE the update game state
+    delete gameState[socket.id];
+    io.emit("gameState updated", gameState);
+  });
+});
--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,14 +1,79 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import App from "./App";
 import useNoSleep from "use-no-sleep";
 import GlobalStateProvider from "./GlobalStateProvider";
 import Scoreboard from "./Scoreboard";
+import { io, Socket } from "socket.io-client";
+// import { socket } from "./socket";
+
+interface GameObject {
+  count: number;
+}
+
+interface GameState {
+  [socketId: string]: GameObject;
+}
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+  ["gameState updated"]: (gameStateFromServer: GameState) => void;
+}
+
+interface ClientToServerEvents {
+  ["counter clicked"]: (socketId: string) => void;
+  ["new player joined"]: () => void;
+  ["a player disconnected"]: () => void;
+}
+
+// storing socket connection in this global variable
+let socket: Socket<ServerToClientEvents, ClientToServerEvents> | null = null;
 
 const AppContainer = () => {
   useNoSleep(true);
+
+  const [gameState, setGameState] = useState<GameState | null>(null);
+
+  useEffect(() => {
+    // connect to the socket server
+    socket = io("ws://localhost:5000");
+
+    // when connected, look for when the server emits the updated count
+    socket.on("gameState updated", function (gameStateFromServer: GameState) {
+      // set the new count on the client
+      setGameState(gameStateFromServer);
+    });
+
+    socket.on("connect", () => {
+      socket?.emit("new player joined");
+    });
+
+    function cleanup() {
+      socket?.emit("a player disconnected");
+    }
+
+    socket.on("disconnect", () => {
+      cleanup();
+    });
+
+    window.addEventListener("beforeunload", cleanup);
+
+    return () => {
+      window.addEventListener("beforeunload", cleanup);
+    };
+  }, []);
+
+  function handleClick() {
+    socket?.emit("counter clicked", socket.id);
+  }
+
   return (
     <GlobalStateProvider>
+      <button onClick={handleClick} className="text-white">
+        State: {JSON.stringify(gameState)}
+      </button>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/scoreboard" element={<Scoreboard />} />
