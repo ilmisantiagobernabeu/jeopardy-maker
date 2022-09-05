9b3550eb612decdbb33b2dfc866653227ee2b8f1
sdennett55@gmail.com <sdennett55@gmail.com>
2022-09-05 00:49:15 -0400

    getting really close to an MVP

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 // import { GameState } from "../stateTypes";
+const { data } = require("./data");
 
 const io = require("socket.io")(5000, {
   cors: {
@@ -9,7 +10,13 @@ const io = require("socket.io")(5000, {
 });
 
 // the game state
-let gameState = { players: {} };
+let gameState = {
+  isBuzzerActive: false,
+  activePlayer: null,
+  players: {},
+  gameBoard: data,
+  incorrectGuesses: [],
+};
 
 io.on("connect", function (socket) {
   console.log("A new client has joined", socket.id);
@@ -25,7 +32,7 @@ io.on("connect", function (socket) {
   // we listen for this event from the clients
   socket.on("counter clicked", (socketId) => {
     gameState.players[socketId].count += 1;
-    console.log("clicked!", socketId, gameState);
+    // console.log("clicked!", socketId, gameState);
 
     // emit to EVERYONE the update game state
     io.emit("gameState updated", gameState);
@@ -33,24 +40,51 @@ io.on("connect", function (socket) {
 
   socket.on("new player joined", () => {
     // emit to EVERYONE the update game state
+    // console.log("hahahahaha");
     io.emit("gameState updated", gameState);
   });
 
   socket.on("a player disconnected", () => {
     // emit to EVERYONE the update game state
-    console.log("wtfwtfwtf player left!!!", socket.id);
+    // console.log("wtfwtfwtf player left!!!", socket.id);
     delete gameState.players[socket.id];
     io.emit("gameState updated", gameState);
   });
 
   socket.on("player signed up", (playerName) => {
-    console.log("yo, this player signed up", playerName);
     gameState.players[socket.id].name = playerName;
+    // console.log("yo, this player signed up", gameState);
     io.emit("gameState updated", gameState);
   });
 
   socket.on("give updated game state", () => {
-    console.log("wtfwtfwtf");
+    // console.log("wtfwtfwtf");
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("A player answers the clue", ({ value: score, clueText }) => {
+    if (score < 0) {
+      gameState.incorrectGuesses.push(gameState.activePlayer);
+      gameState.isBuzzerActive = true;
+    } else {
+      gameState.incorrectGuesses = [];
+      gameState.isBuzzerActive = false;
+      // gameState.gameBoardclueText
+    }
+    gameState.players[gameState.activePlayer].score += score;
+    gameState.activePlayer = null;
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("Host activates the buzzers", () => {
+    gameState.isBuzzerActive = !gameState.isBuzzerActive;
+    io.emit("gameState updated", gameState);
+  });
+
+  socket.on("A player hits the buzzer", () => {
+    gameState.activePlayer = socket.id;
+    gameState.isBuzzerActive = false;
+    console.log("players hits the buzzer", gameState);
     io.emit("gameState updated", gameState);
   });
 
