700cc01223e7043dc8578367aa3b87d3782da3d0
Steve Dennett <sdennett55@gmail.com>
2023-10-09 10:44:53 -0400

    add localStorage to debug page

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -1,44 +1,75 @@
 import { useGlobalState } from "./GlobalStateProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import cx from "classnames";
 
 export const Debug = () => {
   const { gameState } = useGlobalState();
+  const localDebugKeys = localStorage.getItem("bz-debugKeys")
+    ? JSON.parse(localStorage.getItem("bz-debugKeys") as string)
+    : [];
   const initialKeys = Object.keys(gameState || {});
-  const [keys, setKeys] = useState<string[]>(initialKeys);
+  const [keys, setKeys] = useState<string[]>(localDebugKeys || initialKeys);
+
+  // useEffect(() => {
+  //   const newKeys = Object.keys(gameState || {});
+  //   setKeys(newKeys);
+  // }, [gameState]);
+
+  useEffect(() => {
+    localStorage.setItem("bz-debugKeys", JSON.stringify(keys));
+  }, [keys.length]);
 
   if (!gameState) {
     return null;
   }
 
-  const { games, gameBoard, ...rest } = gameState || {};
-
   return (
     <>
-      <div className="flex gap-2">
-        {initialKeys.map((key) => (
-          <button
-            className={cx("text-white px-2 py-1 bg-[#060ce9]", {
-              "bg-red-400": !keys.includes(key),
-            })}
-            onClick={() => {
-              if (keys?.includes(key)) {
-                setKeys((prevKeys) =>
-                  prevKeys.filter((prevKey) => prevKey !== key)
-                );
-              } else {
-                setKeys((prevKeys) => [...prevKeys, key]);
-              }
-            }}
-          >
-            {key}
-          </button>
-        ))}
+      <div className="flex gap-2 text-white items-center">
+        {initialKeys
+          .sort((a, b) => (a > b ? 1 : -1))
+          .map((key) => (
+            <button
+              key={key}
+              className={cx("text-white px-2 py-1", {
+                "bg-[#707079]": !keys.includes(key),
+                "bg-[#060ce9]": keys.includes(key),
+              })}
+              onClick={() => {
+                if (keys?.includes(key)) {
+                  setKeys((prevKeys) =>
+                    prevKeys.filter((prevKey) => prevKey !== key)
+                  );
+                } else {
+                  setKeys((prevKeys) => [...prevKeys, key]);
+                }
+              }}
+            >
+              {key}
+            </button>
+          ))}
+        <button
+          className="ml-2"
+          onClick={() => {
+            setKeys(Object.keys(gameState || {}));
+          }}
+        >
+          Select all
+        </button>
+        <button
+          onClick={() => {
+            setKeys([]);
+          }}
+        >
+          Select none
+        </button>
       </div>
       <pre className="text-white">
         {JSON.stringify(
           Object.fromEntries(
-            Object.entries(gameState).filter(([key]) => keys.includes(key))
+            Object.entries(gameState)
+              .sort((a, b) => (a > b ? 1 : -1))
+              .filter(([key]) => keys.includes(key))
           ),
           null,
           2
