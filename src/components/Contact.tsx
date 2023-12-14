37ec3650b96255a4084c7dc93bb63887aa8031a8
Steve Dennett <sdennett55@gmail.com>
2023-12-14 10:38:00 -0500

    show thanks after contact submit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { PageWrapper } from "./PageWrapper";
 import { useGlobalState } from "./GlobalStateProvider";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Contact = () => {
   const { session } = useGlobalState();
@@ -9,6 +10,7 @@ export const Contact = () => {
   const [feedback, setFeedback] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [amRobot, setAmRobot] = useState(true);
+  const [showThanks, setShowThanks] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,6 +20,7 @@ export const Contact = () => {
         email,
         body: feedback,
       });
+      setShowThanks(true);
     } catch (error) {
       console.error("Error fetching users:", error);
     }
@@ -30,52 +33,65 @@ export const Contact = () => {
         <h2 className="font-bold text-2xl leading-none text-center normal-case mb-2">
           Contact
         </h2>
-        <p className="text-center normal-case text-lg mb-4">
-          Please let us know how to make Buzzinga better!
-        </p>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="text"
-              className="appearance-none rounded-sm p-2 text-base text-black"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+        {showThanks ? (
+          <div className="flex flex-col gap-2">
+            <p className="text-center normal-case text-lg mb-4">
+              Thanks! We'll get back to you soon.
+            </p>
+            <Link className="primary-btn" to="/">
+              Back to homepage
+            </Link>
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="feedback">
-              Feedback <span className="text-red-500">*</span>
-            </label>
-            <textarea
-              id="feedback"
-              className="min-h-[200px] appearance-none rounded-sm p-2 text-base text-black"
-              value={feedback}
-              onChange={(e) => setFeedback(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex gap-2">
-            <input
-              type="checkbox"
-              checked={!amRobot}
-              onChange={() => setAmRobot((prev) => !prev)}
-              required
-            />
-            <label htmlFor="robot">
-              I hereby certify that I am not a robot{" "}
-              <span className="text-red-500">*</span>
-            </label>
-          </div>
-          <button
-            type="submit"
-            className="primary-btn"
-            disabled={amRobot || isLoading || !email || !feedback}
-          >
-            Send
-          </button>
-        </form>
+        ) : (
+          <>
+            <p className="text-center normal-case text-lg mb-4">
+              Please let us know how to make Buzzinga better!
+            </p>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  type="text"
+                  className="appearance-none rounded-sm p-2 text-base text-black"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="feedback">
+                  Feedback <span className="text-red-500">*</span>
+                </label>
+                <textarea
+                  id="feedback"
+                  className="min-h-[200px] appearance-none rounded-sm p-2 text-base text-black"
+                  value={feedback}
+                  onChange={(e) => setFeedback(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex gap-2">
+                <input
+                  type="checkbox"
+                  checked={!amRobot}
+                  onChange={() => setAmRobot((prev) => !prev)}
+                  required
+                />
+                <label htmlFor="robot">
+                  I hereby certify that I am not a robot{" "}
+                  <span className="text-red-500">*</span>
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="primary-btn"
+                disabled={amRobot || isLoading || !email || !feedback}
+              >
+                Send
+              </button>
+            </form>
+          </>
+        )}
       </div>
     </PageWrapper>
   );
