8f534c004d1930603c5bb5dc80f2116a80fbc0a1
Steven Dennett <sdennett55@gmail.com>
2024-01-11 13:57:51 -0500

    Fixing QR code scaling on Safari

--- src/components/QR.tsx
+++ src/components/QR.tsx
@@ -25,7 +25,6 @@ export const QRCode = ({ className = "" }: { className?: string }) => {
       <QRCodeLib
         value={getUrl(gameState?.guid || "")}
         className="w-full h-full max-w-fit"
-        preserveAspectRatio="xMidYMid slice"
       />
     </div>
   );
